cc3b51c7d66a051c7af09e60bcc9913df8c88e6c
Hafiz <hafiz@braintreex.com>
2019-12-28 23:03:09 +0800

    Edit configuration for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,18 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+//const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const fs = require('fs');
+//const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+require('dotenv').config()
+
+const mnemonic = process.env.MNEMONIC;
+const infuraKey = process.env.INFURA_KEY;
+
 
 module.exports = {
   /**
@@ -72,6 +79,13 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,    
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
 
     // Useful for private networks
     // private: {
